e9988c40b32827e72e32ffa778cfeb38f4bf2c61
Entropie67 <olivier.elophe@gmail.com>
2023-01-17 09:56:32 +0100

    last

--- Version_2023/js/script.js
+++ Version_2023/js/script.js
@@ -15,19 +15,20 @@ console.log(tab);
 
 let main = document.getElementById("main");
 
-for (let i = 0 ; i < nbCartes ; i++){
+for (let i = 0 ; i < nbCartes * 2 ; i++){
     main.innerHTML += `<img id ="image${i}" src="images/dos.png" alt="une image">`;
 }
+let cliquees = [];
 
-for (let j = 0 ; j < nbCartes ; j ++){
+for (let j = 0 ; j < nbCartes * 2 ; j ++){
     let carte = document.getElementById(`image${j}`);
     carte.addEventListener("click", (e) => {
         if (carte.src.slice(-7) == "dos.png") {
-            carte.style.border = "solid red";
             carte.src = `images/${tab[j]}`;
+            cliquees.push(carte);
         }else {
             carte.src = "images/dos.png";
         }
-
+        console.log(cliquees);
     });
 }
