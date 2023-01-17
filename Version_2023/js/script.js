2ee00de7294d7d33692acc920e31f7376bb55421
Entropie67 <olivier.elophe@gmail.com>
2023-01-17 09:01:45 +0100

    addevent

--- Version_2023/js/script.js
+++ Version_2023/js/script.js
@@ -1,6 +1,15 @@
 // Fichier script.js
 
 let main = document.getElementById("main");
+let nbCartes = 12;
+for (let i = 0 ; i < nbCartes ; i++){
+    main.innerHTML += `<img id ="image${i}" src="images/dos.png" alt="une image">`;
+}
 
-main.innerHTML = '<img src="images/dos.png" alt="une image">';
-main.style.width = "100px";
+for (let j = 0 ; j < nbCartes ; j ++){
+    let carte = document.getElementById(`image${j}`);
+    carte.addEventListener("click", (e) => {
+        carte.style.border = "solid red";
+
+    });
+}
